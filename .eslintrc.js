748c36b3fee14ef47f894c5950eeb038f9d5dfab
MarkTuwen <m18698577021@163.com>
2024-01-31 00:10:21 +0800

    prepare the layout and add header component

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,8 @@ module.exports = {
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    // Automatically select the type of endOfLine according to file content (CRLF \r\n/LF \n)
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
 }
---
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import classes from './index.module.scss'
+import { Footer } from '../../../../payload/payload-types'
+
+function FooterComponent({ footer }: Readonly<{ footer: Footer }>) {
+  return <div>Footer</div>
+}
+
+export default FooterComponent
